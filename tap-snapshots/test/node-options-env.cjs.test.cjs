8c6672fff04015a931104b81982b24cf8bad7f0a
isaacs <i@izs.me>
2022-03-11 14:50:48 -0800

    tests for node-options-env

---
+++ tap-snapshots/test/node-options-env.cjs.test.cjs
@@ -0,0 +1,166 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/node-options-env.cjs TAP > args has both 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > args has cjs absolute 1`] = `
+"--require={CJSABS}"
+`
+
+exports[`test/node-options-env.cjs TAP > args has cjs relative 1`] = `
+"--require={CJSABS}"
+`
+
+exports[`test/node-options-env.cjs TAP > args has esm absolute 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > args has esm relative 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > args has esm url 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > args has esm url = 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > args loader missing 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > args loader not found 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > empty 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > exp args has esm absolute 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > exp args has esm relative 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > exp args has esm url 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > exp args has esm url = 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > exp opts has esm absolute 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > exp opts has esm relative 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > exp opts has esm url 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > exp opts has esm url = 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > has NO 1`] = `
+"--x" "y" "-z" "--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > has argv 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > has both 1`] = `
+"--x" "y" "-z" "--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > multiple loaders 1`] = `
+"--loader={ESMURL}?{CWD}%2Ftest%2Fnode-options-env.cjs&{CWD}%2Ftest%2Findex.cjs"
+`
+
+exports[`test/node-options-env.cjs TAP > opts cjs, args cjs 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > opts esm, args cjs 1`] = `
+"-r" "{CJSREL}" "--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > opts has both 1`] = `
+"-r" "{CJSREL}" "--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > opts has cjs absolute 1`] = `
+"--require" "{CJSABS}"
+`
+
+exports[`test/node-options-env.cjs TAP > opts has cjs relative 1`] = `
+"--require={CJSREL}"
+`
+
+exports[`test/node-options-env.cjs TAP > opts has esm absolute 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > opts has esm relative 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > opts has esm url 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > opts has esm url = 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > opts loader missing 1`] = `
+"--loader" "--loader" "--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > opts loader not found 1`] = `
+"--loader={ESMURL}?not%20foud"
+`
+
+exports[`test/node-options-env.cjs TAP > other loader in args 1`] = `
+"--loader={ESMURL}"
+`
+
+exports[`test/node-options-env.cjs TAP > other loader in both 1`] = `
+"--loader={ESMURL}?{CWD}%2Ftest%2Fnode-options-env.cjs"
+`
+
+exports[`test/node-options-env.cjs TAP > other loader in opts 1`] = `
+"--loader={ESMURL}?{CWD}%2Ftest%2Fnode-options-env.cjs"
+`
+
+exports[`test/node-options-env.cjs TAP > short args has cjs absolute 1`] = `
+"--require={CJSABS}"
+`
+
+exports[`test/node-options-env.cjs TAP > short args has cjs relative 1`] = `
+"--require={CJSABS}"
+`
+
+exports[`test/node-options-env.cjs TAP > short opts has cjs absolute 1`] = `
+"-r" "{CJSABS}"
+`
+
+exports[`test/node-options-env.cjs TAP > short opts has cjs relative 1`] = `
+"-r" "{CJSREL}"
+`
