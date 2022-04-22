5e13bfd4f54679afeb731d92aad1d35cd8acd784
isaacs <i@izs.me>
2022-04-21 19:53:27 -0700

    test for cjs.cjs

--- test/cjs.cjs
+++ test/cjs.cjs
@@ -1,2 +1,9 @@
 const t = require('tap')
-const loader = require('../lib/cjs.cjs')
+const {getProcessInfo} = require('../lib/get-process-info.cjs')
+const processInfo = getProcessInfo()
+require('../lib/cjs.cjs')
+require('../lib/json-file.cjs')
+const dir = t.testdir({ 'file.js': 'module.exports = "hello"' })
+t.equal(require(dir + '/file.js'), 'hello')
+
+t.same(processInfo.files, [require.resolve('../lib/json-file.cjs'), require.resolve(dir + '/file.js')])
